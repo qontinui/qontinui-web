06f221776eb4913995c883a044fce2ad0aa907e9
Joshua Spinak <jspinak@hotmail.com>
2025-10-07 20:12:22 +0200

    Add force-dynamic to automation-builder page

--- frontend/src/app/(app)/automation-builder/page.tsx
+++ frontend/src/app/(app)/automation-builder/page.tsx
@@ -2,6 +2,8 @@
 
 import AutomationBuilder from "@/components/automation-builder";
 
+export const dynamic = 'force-dynamic'
+
 export default function AutomationBuilderPage() {
   return <AutomationBuilder />;
 }
