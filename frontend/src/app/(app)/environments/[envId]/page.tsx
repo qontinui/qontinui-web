31cb12b91eb8ba2e99060c6b194d2f325839f94c
jspinak <jspinak@hotmail.com>
2026-08-05 16:25:29 +0200

    fix: allow scrolling on environment detail page when panels expand

--- frontend/src/app/(app)/environments/[envId]/page.tsx
+++ frontend/src/app/(app)/environments/[envId]/page.tsx
@@ -191,7 +191,7 @@ export default function EnvironmentDetailPage() {
   }
 
   return (
-    <div className="p-6 space-y-6">
+    <div className="h-full overflow-y-auto p-6 space-y-6">
       <div>
         <Link
           href="/environments"
